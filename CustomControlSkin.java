634f06663d0635bd37be23f5f6dbcb4abcac34cc
unknown <remipiubello@gmail.com>
2017-12-01 22:27:13 +0100

    prout

--- CustomControlSkin.java
+++
@@ -1,9 +0,0 @@
-import javafx.scene.control.Skin;
-import javafx.scene.control.SkinBase;
-
-public class CustomControlSkin extends SkinBase<CustomControl> implements Skin<CustomControl> {
-    // Default constructor
-    public CustomControlSkin(CustomControl gc) {
-        super(gc);
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Go
-Game of go with javafx
